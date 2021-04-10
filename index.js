e10a22907215f3554c115faa780ae8fab5543a49
tiagopaes <tiagopaes37@gmail.com>
2021-04-09 22:00:58 -0300

    Refactoring to use env vars and make the code more dynamic

--- index.js
+++ index.js
@@ -1,59 +1,71 @@
 require('dotenv').config();
 const axios = require('axios');
+const {
+  TELEGRAM_BOT_TOKEN,
+  TELEGRAM_CHAT_ID_TO_NOTIFY,
+  TELEGRAM_API_URL,
+  REMOTEOK_API_URL,
+  REMOTEOK_WEBSITE_URL,
+  JOB_TAGS,
+  JOBS_PER_TAG,
+} = process.env;
 
-const run = async () => {
+run();
+
+async function run() {
   try {
-    const apiUrl = 'https://remoteok.io/api?tag=dev';
-    const userAgent =
-      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36';
-    const options = { headers: { 'User-Agent': userAgent } };
-    const response = await axios.get(apiUrl, options);
+    const jobs = await getJobs();
+    const tags = JOB_TAGS.split(',');
 
-    const jobs = await response.data;
-    jobs.shift();
+    let text = "Today's Remote Jobs\n\n";
+    tags.forEach((tag) => {
+      text += buildText(jobs, tag);
+      text += '\n';
+    });
+    text += `To see the full list, visit ${REMOTEOK_WEBSITE_URL}`;
 
-    let text = "Today's Remote Jobs\n\n\n";
-    text += 'Mobile\n';
-    text += '==============================\n';
-    jobs
-      .filter((job) => job.tags.includes('mobile'))
-      .slice(0, 5)
-      .forEach((job) => {
-        text += `${job.position} at ${job.company} \n`;
-        text += `${job.url} \n\n`;
-      });
-
-    text += '\nFrontend\n';
-    text += '==============================\n';
-    jobs
-      .filter((job) => job.tags.includes('frontend'))
-      .slice(0, 5)
-      .forEach((job) => {
-        text += `${job.position} at ${job.company} \n`;
-        text += `${job.url} \n\n`;
-      });
-
-    text += '\nBackend\n';
-    text += '==============================\n';
-    jobs
-      .filter((job) => job.tags.includes('backend'))
-      .slice(0, 5)
-      .forEach((job) => {
-        text += `${job.position} at ${job.company} \n`;
-        text += `${job.url} \n\n`;
-      });
-    text += '\nTo access the complete list access https://remoteok.io';
-
-    const telegramUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`;
-    const data = {
-      chat_id: '@remotejobsfordevs',
-      disable_web_page_preview: true,
-      text
-    };
-    await axios.post(telegramUrl, data);
+    await sendMessage(text);
   } catch (err) {
     console.log(err);
   }
-};
+}
 
-run();
+async function getJobs() {
+  const userAgent =
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36';
+  const options = { headers: { 'User-Agent': userAgent } };
+  const response = await axios.get(`${REMOTEOK_API_URL}?tag=dev`, options);
+
+  const jobs = response.data;
+  jobs.shift();
+
+  return jobs;
+}
+
+function buildText(jobs, tag) {
+  let text = capitalize(tag) + '\n';
+  text += '==============================\n';
+  jobs
+    .filter((job) => job.tags.includes(tag))
+    .slice(0, JOBS_PER_TAG)
+    .forEach((job) => {
+      text += `${job.position} at ${job.company} \n`;
+      text += `${job.url} \n\n`;
+    });
+
+  return text;
+}
+
+function capitalize(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+async function sendMessage(text) {
+  const url = `${TELEGRAM_API_URL}/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+  const data = {
+    chat_id: TELEGRAM_CHAT_ID_TO_NOTIFY,
+    disable_web_page_preview: true,
+    text,
+  };
+  await axios.post(url, data);
+}
